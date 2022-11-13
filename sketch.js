9ffd6abf279a3b68454e57e40cee22b0a015ac86
noni2005 <83501411+noni2005@users.noreply.github.com>
2022-11-13 15:26:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,113 +4,65 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
+var backgroundImg;
+var hour;
 
-var gameState = "onSling";
-var bg ="sprites/bg.png";
-var score =0 ;
+var bg = "sunrise.png";
+
 function preload() {
-   getBackgroundImg(); 
+    getBackgroundImg();
+    
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1200,700);
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    if(backgroundImg){
+    if(backgroundImg)
         background(backgroundImg);
 
-    }
-   
-
-    noStroke();
-    textSize (35);
-    fill("white");
-    text('Score '+score,width-300,50);
     Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
-}
-
-function mouseDragged(){
-    if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    fill("black");
+    textSize(30);
+    
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+    }else if(hour==0){
+        text("Time : 12 AM",100,100);
+    }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
     }
+
 }
 
-
-function mouseReleased(){
-    slingshot.fly();
-    gameState = "launched";
-}
-
-function keyPressed(){
-    if(keyCode === 32){
-       // slingshot.attach(bird.body);
-    }
-}
 async function getBackgroundImg(){
-    var response=await fetch('https://worldtimeapi.org/api/timezone/Asia/Kolkata');
-    var responseJSON=await response.json();
 
-    var dateTime=responseJSON.datetime;
-    var hour=dateTime.slice(11,13);
-    console.log('time hua hai   ***********'+ hour);
-    if(hour>=06 && hour<=16){
-        bg='sprites/bg.png';
+    // write code to fetch time from API
+    
+ 
+    //change the data in JSON format and store it in variable responseJSON
+    
 
+    
+    //fetch datetime from responseJSON
+    
+    
+
+    // slice the datetime to extract hour
+    
+
+    
+    if(hour>=0 && hour<18 ){
+        bg = "sunrise.png";
     }
     else{
-        bg='sprites/bg2.jpg';
-
+        bg="sunset.png"
     }
-    backgroundImg=loadImage(bg);
-    console.log(backgroundImg)
+    
+    backgroundImg = loadImage(bg);
 }
